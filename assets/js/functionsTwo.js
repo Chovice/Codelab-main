0e94e8911220d664ccbf2e195947f42b6fe49ad4
Chovice <tobi5340@elev.techcollege.dk>
2025-03-12 14:20:20 +0100

    Initial commit

---
+++ assets/js/functionsTwo.js
@@ -0,0 +1,18 @@
+
+
+// her kaldes, eller invokes din funktion
+
+
+helloWorld("hello world", "med parameter");
+
+
+// her skal du skrive din funktion
+function helloWorld(minVærdi , minAndenValue ) {
+    let result = minVærdi + " " + minAndenValue ;
+    console.log(result)
+}
+ 
+ 
+
+ 
+
